c987d776f95638febf8bca5baecca8de52eaeb9a
NateBuckley <albinoblacksheep0@yahoo.com>
2022-06-07 14:01:49 -0500

    completed from 11:47 to 1:13

--- JavaAssessment/src/main/java/com/hexaware/MainDriver.java
+++ JavaAssessment/src/main/java/com/hexaware/MainDriver.java
@@ -1,11 +1,13 @@
 package com.hexaware;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
+import java.util.Comparator;
 
 import com.hexaware.models.Employee;
 
@@ -35,16 +37,87 @@ public class MainDriver {
 		//List down the names of all employees in each department
 		System.out.printf("%nList of all employees in each department:%n");
 		Map<String, List<Employee>> employeeNamesByDepartment =
-		         empList.stream().collect(Collectors.groupingBy(Employee::getDepartment));
+				empList.stream().collect(Collectors.groupingBy(Employee::getDepartment));
 		
 		employeeNamesByDepartment.forEach((department, employeesInDepartment) -> 
-		         {
-		        	System.out.printf("%n--------------------------------------%n");
-		            System.out.println(department);
-		            System.out.println("--------------------------------------");
-		            employeesInDepartment.forEach(employee -> System.out.println(employee.getName()));
-		         }
-		      );
+			{
+				System.out.printf("%n--------------------------------------%n");
+		        System.out.println(department);
+		        System.out.println("--------------------------------------");
+		        employeesInDepartment.forEach(employee -> System.out.println(employee.getName()));
+			}
+		);
+		
+		List<Integer> intList1 = new ArrayList<>();
+		
+		intList1.add(5);
+		intList1.add(13);
+		intList1.add(4);
+		intList1.add(21);
+		intList1.add(13);
+		intList1.add(27);
+		intList1.add(2);
+		intList1.add(59);
+		intList1.add(59);
+		intList1.add(34);
+		
+		//List out the duplicates
+		Set<Integer> elementSet = new HashSet<>();
+
+		System.out.printf("%nList of all duplicates:%n");
+		intList1.stream().filter(n -> !elementSet.add(n)).collect(Collectors.toSet()).forEach(System.out::println);
+		
+		List<Integer> intList2 = new ArrayList<>();
+		
+		intList2.add(4);
+		intList2.add(5);
+		intList2.add(7);
+		intList2.add(8);
+		intList2.add(99);
+		intList2.add(100);
+		intList2.add(101);
+		intList2.add(33);
+		intList2.add(32);
+		intList2.add(4);
+		intList2.add(4);
+		
+		//Remove all duplicates without Set
+		List<Integer> listWithoutDuplicates = intList2.stream().distinct().collect(Collectors.toList());
+		
+		System.out.printf("%nList with all duplicates removed:%n");
+		System.out.println(listWithoutDuplicates);
+		
+		List<Integer> intList3 = new ArrayList<>();
+		
+		intList3.add(3);
+		intList3.add(90);
+		intList3.add(350);
+		intList3.add(5);
+		
+		//Get the max value of list
+		Integer maxValue = intList3.stream().max(Comparator.comparing(Integer::valueOf)).get();
+		System.out.printf("%nMax value of list:%n");
+		System.out.println(maxValue);
+		
+		List<Integer> intList4 = new ArrayList<>();
+		
+		intList4.add(6);
+		intList4.add(8);
+		intList4.add(3);
+		intList4.add(5);
+		intList4.add(1);
+		intList4.add(9);
+		
+		//Get max and min values of list
+		System.out.printf("%nMax and values of list:%n");
+		
+		//max
+		maxValue = intList4.stream().max(Comparator.comparing(Integer::valueOf)).get();
+		//min
+		Integer minValue = intList4.stream().min(Comparator.comparing(Integer::valueOf)).get();
+		
+		System.out.println("Max: " + maxValue);
+		System.out.println("Min: " + minValue);
 	}
 
 }
